396365bed96ee6116dc380ae94becb649d0916e2
Alexander Obi <alexebube@gmail.com>
2013-09-11 14:02:41 -0400

    Added code for sub navigation from the page table in the database

--- content.php
+++ content.php
@@ -4,25 +4,34 @@
 			<table id="structure">
 				<tr>
 					<td id="navigation">
+                                            <ul class="subjects">
 						<?php
-                                                    // 3. Perform database query
-                                                    $result = mysql_query("SELECT * FROM subjects", $connection);
-                                                    if (!$result) {
+                                                    // Perform database query
+                                                    $subject_set = mysql_query("SELECT * FROM subjects", $connection);
+                                                    if (!$subject_set) {
                                                             die("Database query failed: " . mysql_error());
                                                     }
 
-                                                    // 4. Use returned data
-                                                    while ($row = mysql_fetch_array($result)) {
-                                                            echo $row["menu_name"]." ".$row["position"]."<br />";
+                                                    // Use returned data
+                                                    while ($subject = mysql_fetch_array($subject_set)) {
+                                                        echo "<li>{$subject["menu_name"]}</li>";
+                                                        $page_set = mysql_query("SELECT * FROM pages WHERE subject_id = {$subject["id"]}", $connection);
+                                                        if (!$page_set) {
+                                                                die("Database query failed: " . mysql_error());
+                                                        }
+                                                        
+                                                        echo "<ul class=\"pages\">";
+                                                        while ($page = mysql_fetch_array($page_set)) {
+                                                            echo "<li>{$page["menu_name"]}</li>";
+                                                        }
+                                                        echo "</ul>";
                                                     }
-
-                                                ?>
-
+                                              ?>
+                                            </ul>     
 					</td>
 					<td id="page">
 						<h2>Content Area</h2>
-						
 					</td>
 				</tr>
 			</table>
-<?php require './includes/footer.php';?>
+<?php include ("includes/footer.php"); ?>
