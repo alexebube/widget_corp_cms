ce033e3ef068a6c3d3f52120631206e5217f68e0
Alexander Obi <alexebube@gmail.com>
2013-09-11 15:23:03 -0400

    Created a new function confirm_query() in the functions.php file for checking the correctness of mysql query

--- includes/functions.php
+++ includes/functions.php
@@ -3,4 +3,10 @@
 /*
  * This file contains all functions for this project
  */
+
+function confirm_query($result_set) {
+    if (!$result_set) {
+        die("Database query failed: " . mysql_error());
+    }
+}
 ?>
