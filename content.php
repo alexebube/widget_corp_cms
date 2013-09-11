65cc253c4c5ef986ee6e15566abc2a40f856bb8e
Alexander Obi <alexebube@gmail.com>
2013-09-11 13:28:17 -0400

    Modified content.php to include navigation items to be stayled latter

--- content.php
+++ content.php
@@ -1,19 +1,28 @@
-<?php require_once './includes/functions.php'; ?>
-<?php include './includes/header.php'; ?>
+<?php require_once ("includes/connections.php"); ?>
+<?php require_once ("includes/functions.php"); ?>
+<?php include ("includes/header.php"); ?>
 			<table id="structure">
 				<tr>
 					<td id="navigation">
-						&nbsp;
+						<?php
+                                                    // 3. Perform database query
+                                                    $result = mysql_query("SELECT * FROM subjects", $connection);
+                                                    if (!$result) {
+                                                            die("Database query failed: " . mysql_error());
+                                                    }
+
+                                                    // 4. Use returned data
+                                                    while ($row = mysql_fetch_array($result)) {
+                                                            echo $row["menu_name"]." ".$row["position"]."<br />";
+                                                    }
+
+                                                ?>
+
 					</td>
 					<td id="page">
-						<h2>Staff Menu</h2>
-						<p>Welcome to the staff area.</p>
-						<ul>
-							<li><a href="content.php">Manage Website Content</a></li>
-							<li><a href="new_user.php">Add Staff User</a></li>
-							<li><a href="logout.php">Logout</a></li>
-						</ul>
+						<h2>Content Area</h2>
+						
 					</td>
 				</tr>
 			</table>
-<?php include './includes/footer.php';?>
+<?php require './includes/footer.php';?>
