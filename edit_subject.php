473c1785e80510ee0712cf40301dfe584e668d24
Alexander Obi <alexebube@gmail.com>
2013-10-02 12:49:01 -0400

    Created a new file edit_subject.php to handle editing and modifying subjects

---
+++ edit_subject.php
@@ -0,0 +1,43 @@
+<?php require_once ("includes/connections.php"); ?>
+<?php require_once ("includes/functions.php"); ?>
+<?php find_selected_page(); ?>
+<?php include ("includes/header.php"); ?>
+			<table id="structure">
+				<tr>
+					<td id="navigation">
+                                            <?php echo navigation($sel_subject, $sel_page); ?>
+					</td>
+					<td id="page">
+                                            <h2>Edit Subject: <?php echo $sel_subject['menu_name']; ?></h2>
+                                            <form action="edit_subject.php?subj=<?php echo urlencode($sel_subject['id']); ?>" method="post">
+                                                <p>Subject name:
+                                                    <input type="text" name="menu_name" value="<?php echo $sel_subject['menu_name'] ?>" id="menu_name" />
+                                                </p>
+                                                <p>Position:
+                                                    <select name="position">
+                                                        <?php 
+                                                            $subject_set = get_all_subjects();
+                                                            $subject_count = mysql_num_rows($subject_set);
+                                                            for($count=1; $count<=$subject_count+1; $count++){
+                                                                echo "<option value=\"{$count}\"";
+                                                                    if($sel_subject['position']== $count)
+                                                                        echo " selected";
+                                                                echo ">{$count}</option>";
+                                                            }
+                                                        ?>
+                                                    </select>
+                                                </p>
+                                                <p>Visible:
+                                                    <input type="radio" name="visible" value="0" />No
+                                                    &nbsp;
+                                                    <input type="radio" name="visible" value="1" />Yes
+                                                </p>
+                                                <input type="submit" value="Edit Subject" />
+                                            </form>
+                                            
+                                            <br/>
+                                            <a href="content.php">Cancel</a>
+					</td>
+				</tr>
+			</table>
+<?php include ("includes/footer.php"); ?>
